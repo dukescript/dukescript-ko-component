8edbeedb55a6946e38296a40ef3d59a960d358c4
eppleton <info@eppleton.de>
2015-05-15 15:41:29 +0200

    use @JavaScriptRessource to store JS in separate file at Gj's request :-)

--- js/src/main/java/com/dukescript/demos/kocomponent/js/ComponentLikeWidget.java
+++ js/src/main/java/com/dukescript/demos/kocomponent/js/ComponentLikeWidget.java
@@ -1,30 +1,14 @@
 package com.dukescript.demos.kocomponent.js;
 
 import net.java.html.js.JavaScriptBody;
-
+import net.java.html.js.JavaScriptResource;
+@JavaScriptResource(value = "kocomponent.js")
 public final class ComponentLikeWidget {
 
     private ComponentLikeWidget() {
     }
 
-    @JavaScriptBody(args = {  },body = "ko.components.register('like-widget', {\n" +
-"    viewModel: function(params) {\n" +
-"        // Data: value is either null, 'like', or 'dislike'\n" +
-"        this.chosenValue = params.value;\n" +
-"         \n" +
-"        // Behaviors\n" +
-"        this.like = function() { this.chosenValue('like'); }.bind(this);\n" +
-"        this.dislike = function() { this.chosenValue('dislike'); }.bind(this);\n" +
-"    },\n" +
-"    template:\n" +
-"        '<div class=\"like-or-dislike\" data-bind=\"visible: !chosenValue()\">\\\n" +
-"            <button data-bind=\"click: like\">Like it</button>\\\n" +
-"            <button data-bind=\"click: dislike\">Dislike it</button>\\\n" +
-"        </div>\\\n" +
-"        <div class=\"result\" data-bind=\"visible: chosenValue\">\\\n" +
-"            You <strong data-bind=\"text: chosenValue\"></strong> it\\\n" +
-"        </div>'\n" +
-"});")
+    @JavaScriptBody(args = {  },body = "")
     public static native void registerComponent();
 
 }
